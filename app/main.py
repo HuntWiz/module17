a8557b21ee987bd6165d4bb215eb2e979a8c8f9e
HuntWiz <botalov.gleb123@gmail.com>
2024-12-14 05:27:16 +0500

    Main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from app.routers import task, user
+from app.backend.db import engine, Base
 
 app = FastAPI()
 
@@ -11,5 +12,7 @@ async def welcome():
 app.include_router(user.router)
 app.include_router(task.router)
 
+Base.metadata.create_all(bind=engine)
+
 if __name__ == '__main__':
-    uvicorn.run("module_16_5:app", reload=True)
+    uvicorn.run("main:app", reload=True)
